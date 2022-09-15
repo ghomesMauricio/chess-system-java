1e2096c3368b925a1e617802d12e7dc199a2b4e0
ghomesMauricio <ghomes.mauricio@gmail.com>
2022-09-14 21:12:51 -0300

    lugares de pecas no tabuleiro

--- src/Xadrez/Partida_xadrez.java
+++ src/Xadrez/Partida_xadrez.java
@@ -1,5 +1,8 @@
 package Xadrez;
 
+import Xadrez.pecas.Rei;
+import Xadrez.pecas.Torre;
+import tabuleiro_jogo.Posicao;
 import tabuleiro_jogo.Tabuleiro;
 
 public class Partida_xadrez {
@@ -7,13 +10,21 @@ public class Partida_xadrez {
 
     public Partida_xadrez(){
         tabuleiro = new Tabuleiro(8,8);
+        IniciandoPartida();
     }
     public Pecas_xadrez[][] pecas(){
         Pecas_xadrez[][] mat = new Pecas_xadrez[tabuleiro.getFileira()][tabuleiro.getColuna()];
-        for(int F=0; F <tabuleiro.getFileira(); F++){
-            for ( int C=0; C < tabuleiro.getColuna(); C++)
+        for(int F=0; F <tabuleiro.getColuna(); F++){
+            for ( int C=0; C < tabuleiro.getFileira(); C++)
                 mat[F][C] = (Pecas_xadrez) tabuleiro.peca(F,C);
         }
         return mat;
     }
+    private void IniciandoPartida(){
+        tabuleiro.Espaco_peca(new Torre(tabuleiro,Cor.BRANCO),new Posicao(0,0));
+        tabuleiro.Espaco_peca(new Torre(tabuleiro,Cor.BRANCO),new Posicao(0,7));
+        tabuleiro.Espaco_peca(new Rei(tabuleiro,Cor.PRETO),new Posicao(0,4));
+        tabuleiro.Espaco_peca(new Rei(tabuleiro,Cor.PRETO),new Posicao(7,4));
+    }
+
 }
